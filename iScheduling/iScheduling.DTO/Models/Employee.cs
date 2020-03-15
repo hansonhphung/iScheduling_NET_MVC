c2d058eb12260dacb2ed3804199a5decec7a4a41
hansonphung <n01312493@humbermail.ca>
2020-03-15 01:12:28 -0400

    Create DTO project

---
+++ iScheduling/iScheduling.DTO/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iScheduling.DTO.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string AccessToken { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName {
+            get {
+                return string.Format("{0} {1}", FirstName, LastName);
+            }
+        }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ iScheduling/iScheduling.DTO/Models/Shift.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iScheduling.DTO.Models
+{
+    public class Shift
+    {
+        public Guid ShiftId { get; set; }
+        public Guid EmployeeId { get; set; }
+        public Guid AssignedBy { get; set; }
+        public DateTime AssignedAt { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
