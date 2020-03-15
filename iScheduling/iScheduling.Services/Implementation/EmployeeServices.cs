cb0ea5dee3fad1d43120775d2aa29528db0fd377
hansonphung <n01312493@humbermail.ca>
2020-03-15 01:12:39 -0400

    Create Service project

---
+++ iScheduling/iScheduling.Services/Implementation/EmployeeServices.cs
@@ -0,0 +1,25 @@
+﻿using iScheduling.Context;
+using iScheduling.DTO.Models;
+using iScheduling.Repositories.Interface;
+using iScheduling.Services.Implementation;
+using iScheduling.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iScheduling.Implementation.Services
+{
+    public class EmployeeServices : BaseServices, IEmployeeServices
+    {
+        private readonly IEmployeeRepositories employeeRepositories;
+        public EmployeeServices(IEmployeeRepositories _employeeRepository){
+            employeeRepositories = _employeeRepository;
+        }
+
+        public IList<Employee> GetAllEmployees()
+        {
+            return employeeRepositories.GetAllEmployees().Select(x => new Employee()).ToList();
+        }
+    }
+}
