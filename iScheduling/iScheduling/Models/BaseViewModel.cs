64b623991191c9876d2fc150e7ceada88e0aa202
hansonphung <n01312493@humbermail.ca>
2020-03-17 19:20:00 -0400

    Implement custom exception to handle exception

---
+++ iScheduling/iScheduling/Models/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iScheduling.Models
+{
+    public class BaseViewModel<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
