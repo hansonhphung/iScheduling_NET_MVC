91ef9f2de1655de583522294192c3c2a397c6943
hansonphung <n01312493@humbermail.ca>
2020-03-17 19:21:23 -0400

    Add view to add employee as partial view

---
+++ iScheduling/iScheduling/Scripts/app-js/employee.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(() => {
+    getEmployeeInfo = (empId) => {
+        var url = `http://localhost:52901/Employee/Edit?employeeId=${empId}`;
+
+        $.get(url, (data) => {
+            $('#edit-employee-modal').html(data);
+
+            $('#edit-employee').modal('show');
+        });
+    }
+
+    deleteEmployee = (empId) => {
+        var url = `http://localhost:52901/Employee/_Delete?employeeId=${empId}`;
+
+        $.get(url, (data) => {
+            $('#delete-employee-modal').html(data);
+
+            $('#delete-employee').modal('show');
+        });
+    }
+});
+
