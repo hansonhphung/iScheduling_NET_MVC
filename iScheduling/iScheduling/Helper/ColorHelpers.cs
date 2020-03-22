80714060925c8a326ae50e46581ff56a16af3884
hansonphung <n01312493@humbermail.ca>
2020-03-22 16:06:22 -0400

    Implement Color Helpers and string helpers

---
+++ iScheduling/iScheduling/Helper/ColorHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iScheduling.Helper
+{
+    public class ColorHelpers
+    {
+        public static string RandomColor()
+        {
+            return string.Format("#{0}", Guid.NewGuid().ToString().Substring(0, 6));
+        }
+    }
+}
---
+++ iScheduling/iScheduling/Helper/StringHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iScheduling.Helper
+{
+    public static class StringHelpers
+    {
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return s == null || s.Equals(string.Empty);
+        }
+    }
+}
