78787be325d296de174e43458f0b9d2b9ef44c13
hansonphung <n01312493@humbermail.ca>
2020-04-18 02:11:57 -0400

    Update List Shift Employee View to add Request Day Off functionality.

---
+++ iScheduling/iScheduling/Scripts/app-js/day-off-request.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(() => {
+
+    requestDayOff = (shiftId) => {
+        var url = `/DayOffRequest/Create?shiftId=${shiftId}`;
+
+        $.get(url, (data) => {
+            $('#request-day-off-modal').html(data);
+
+            $('#create-day-off-request').modal('show');
+        });
+    }
+
+    createRequest = () => {
+        var shiftId = $('#shiftId').val();
+
+        var reason = $('#txtReason').val();
+
+        var url = `/DayOffRequest/Create`;
+        var data = {
+            ShiftId: shiftId,
+            Reason: reason
+        };
+
+        $.post(url, data, (res) => {
+            if (!res.IsSuccess)
+                alert(res.Message);
+
+            // reload list requests page
+            var selectedEndDate = $('#shiftEndDatePicker').val();
+            $('#shiftEndDatePicker').datepicker("setDate", selectedEndDate);
+
+            $('#create-day-off-request').modal('hide');
+        });
+    }
+
+    approve = (requestId) => {
+        alert('Request approved');
+    }
+
+    reject = (requestId) => {
+        alert('Request rejected');
+    }
+});
